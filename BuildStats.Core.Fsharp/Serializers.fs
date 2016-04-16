21fed01f531d6f838741f531ce1f4e50007e4e99
Dustin Moris Gorski <dustinmoris@gmail.com>
2016-04-16 12:26:51 +0100

    Removed FSharp work in progress (moved to feature branch)

--- BuildStats.Core.Fsharp/Serializers.fs
+++
@@ -1,9 +0,0 @@
-﻿module Serializers
-
-open Newtonsoft.Json
-
-let serializeJson obj =
-    JsonConvert.SerializeObject(obj)
-
-let deserializeJson(json : string) =
-    JsonConvert.DeserializeObject(json)
