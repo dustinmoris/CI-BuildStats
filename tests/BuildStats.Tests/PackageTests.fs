b3b09008e108ffbf43bfd8ab1d66b9a0fbf56a5a
Dustin Moris Gorski <dustinmoris@gmail.com>
2020-04-14 11:52:03 +0100

    Added packageVersion option to NuGet and MyGet badges
    Fixes #28

--- tests/BuildStats.Tests/PackageTests.fs
+++ tests/BuildStats.Tests/PackageTests.fs
@@ -46,91 +46,91 @@ let shouldBeTrue actual =
 
 [<Fact>]
 let ``Lanem returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "Lanem" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "Lanem" false None |> runTask
 
     package.Value.Name        |> shouldEqual "Lanem"
     package.Value.Downloads   |> shouldBeGreaterThan 800
 
 [<Fact>]
 let ``Guardo returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "Guardo" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "Guardo" false None |> runTask
 
     package.Value.Name        |> shouldEqual "Guardo"
     package.Value.Downloads   |> shouldBeGreaterThan 49
 
 [<Fact>]
 let ``Moq returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "Moq" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "Moq" false None |> runTask
 
     package.Value.Name        |> shouldEqual "Moq"
     package.Value.Downloads   |> shouldBeGreaterThan 1874730
 
 [<Fact>]
 let ``NUnit returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "NUnit" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "NUnit" false None |> runTask
 
     package.Value.Name        |> shouldEqual "NUnit"
     package.Value.Downloads   |> shouldBeGreaterThan 1283920
 
 [<Fact>]
 let ``NSubstitute returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "NSubstitute" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "NSubstitute" false None |> runTask
 
     package.Value.Name        |> shouldEqual "NSubstitute"
     package.Value.Downloads   |> shouldBeGreaterThan 661870
 
 [<Fact>]
 let ``jQuery returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "jQuery" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "jQuery" false None |> runTask
 
     package.Value.Name        |> shouldEqual "jQuery"
     package.Value.Downloads   |> shouldBeGreaterThan 4233340
 
 [<Fact>]
 let ``Microsoft.AspNet.Mvc returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "Microsoft.AspNet.Mvc" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "Microsoft.AspNet.Mvc" false None |> runTask
 
     package.Value.Name        |> shouldEqual "Microsoft.AspNet.Mvc"
     package.Value.Downloads   |> shouldBeGreaterThan 2956200
 
 [<Fact>]
 let ``EntityFramework returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "EntityFramework" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "EntityFramework" false None |> runTask
 
     package.Value.Name        |> shouldEqual "EntityFramework"
     package.Value.Downloads   |> shouldBeGreaterThan 4496670
 
 [<Fact>]
 let ``NServiceBus.PostgreSQL PreRelease package returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "NServiceBus.PostgreSQL" true |> runTask
+    let package = NuGet.getPackageAsync httpClient "NServiceBus.PostgreSQL" true None |> runTask
 
     package.Value.Name        |> shouldEqual "NServiceBus.PostgreSQL"
     package.Value.Downloads   |> shouldBeGreaterThan 550
 
 [<Fact>]
 let ``Newtonsoft.Json returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "Newtonsoft.Json" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "Newtonsoft.Json" false None |> runTask
 
     package.Value.Name        |> shouldEqual "Newtonsoft.Json"
     package.Value.Downloads   |> shouldBeGreaterThan 4998550
 
 [<Fact>]
 let ``Paket returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "Paket" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "Paket" false None |> runTask
 
     package.Value.Name        |> shouldEqual "Paket"
     package.Value.Downloads   |> shouldBeGreaterThan 127890
 
 [<Fact>]
 let ``Package written in lowercase returns correct result``() =
-    let package = NuGet.getPackageAsync httpClient "newtonsoft.json" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "newtonsoft.json" false None |> runTask
 
     package.Value.Name        |> shouldEqual "Newtonsoft.Json"
     package.Value.Downloads   |> shouldBeGreaterThan 4998550
 
 [<Fact>]
 let ``Non existing package returns none``() =
-    let package = NuGet.getPackageAsync httpClient "myPackage.which.does.not.exist" false |> runTask
+    let package = NuGet.getPackageAsync httpClient "myPackage.which.does.not.exist" false None |> runTask
 
     package.IsNone |> shouldBeTrue
 
@@ -140,21 +140,21 @@ let ``Non existing package returns none``() =
 
 [<Fact>]
 let ``NEventSocket returns correct result``() =
-    let package = MyGet.getPackageFromOfficialFeedAsync httpClient ("neventsocket-prerelease", "NEventSocket") false |> runTask
+    let package = MyGet.getPackageFromOfficialFeedAsync httpClient ("neventsocket-prerelease", "NEventSocket") false None |> runTask
 
     package.Value.Name        |> shouldEqual "NEventSocket"
     package.Value.Downloads   |> shouldBeGreaterThan 4
 
 [<Fact>]
 let ``MyGet Package written in lowercase returns correct result``() =
-    let package = MyGet.getPackageFromOfficialFeedAsync httpClient ("neventsocket-prerelease", "neventsocket") false |> runTask
+    let package = MyGet.getPackageFromOfficialFeedAsync httpClient ("neventsocket-prerelease", "neventsocket") false None |> runTask
 
     package.Value.Name        |> shouldEqual "NEventSocket"
     package.Value.Downloads   |> shouldBeGreaterThan 4
 
 [<Fact>]
 let ``Non existing MyGet package returns none``() =
-    let package = MyGet.getPackageFromOfficialFeedAsync httpClient ("not-found", "myPackage.which.does.not.exist") false |> runTask
+    let package = MyGet.getPackageFromOfficialFeedAsync httpClient ("not-found", "myPackage.which.does.not.exist") false None |> runTask
 
     package.IsNone |> shouldBeTrue
 
@@ -169,6 +169,7 @@ let ``Microsoft.Bot.Builder returns correct result``() =
             httpClient
             ("botbuilder", "botbuilder-v4-dotnet-daily", "Microsoft.Bot.Builder")
             false
+            None
         |> runTask
 
     package.Value.Name        |> shouldEqual "Microsoft.Bot.Builder"
